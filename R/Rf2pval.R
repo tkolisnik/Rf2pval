2a6553e47ed7c2a23f9a1d98fb046f991601e2a3
Tyler Kolisnik <tkolisnik@gmail.com>
2022-02-22 19:08:44 -0800

    fixes
    Fixes, name changes for clarity, error checking, spelling fixes,

--- R/Rf2pval.R
+++ R/Rf2pval.R
@@ -1,5 +1,5 @@
 #' @details
-#' This package contains a series of functions which help obtain p-values from Random Forest generated gini importance scores.The functions must be used in order. The format of the input dataframes is critical.
+#' This package contains a series of functions which help obtain p-values from Random Forest generated feature importance scores.The functions must be used in order. The format of the input dataframes is critical.
 #' www.github.com/tkolisnik/Rf2pval
 #'
 #'
--- R/loadDatasets.R
+++ R/loadDatasets.R
@@ -7,22 +7,28 @@
 #' @return  A tibble containing all of the feature weight data for the true (observed) dataset.
 #' @export
 load_true_fws <- function(pathtofile,featureWeightColumnName){
+  if(grepl("\\.csv$", pathtofile)){
   d0 <- readr::read_csv(pathtofile,show_col_types=FALSE)
   d2 <- d0 %>% dplyr::mutate(permutation = 0) %>% mutate(featureRank=as.numeric(rownames(d0))) %>% rename(featureWeight= all_of(featureWeightColumnName))
   d3 <- d2 %>% mutate("LogFeatureWeight" = log(featureWeight))
   return(d3)
+  }
+  else{
+    stop("Input file type is not .CSV ; Please check that it is .csv and try again")
+  }
 }
 
 
 
-#' Load feature weights for permutated data
+#' Load feature weights for permuted data
 #'
 #' This function loads the first random forest results files for the 100 randomized outcome variable null permutations
 #'
 #' @param pathtofile Path to a folder containing minimum 100 csv files (one for each null permutation). Input files must be in .csv format as a table of feature weights or Gini Importance scores for each named feature. Must have a minimum of two columns, one named featurename and another named featureweight. The true non-randomized data .csv file must NOT be in this folder.
-#' @return A tibble containing all of the feature weight data for the permutated datasets.
+#' @return A tibble containing all of the feature weight data for the permuted datasets.
 #' @export
-load_permutated_fws <- function(pathtofile,featureWeightColumnName){
+load_permuted_fws <- function(pathtofile,featureWeightColumnName){
+  if(length(list.files(path=pathtofile,pattern=".csv"))>0){
   allfiles<-list.files(path=pathtofile,pattern=".csv",full.names=TRUE)
   dd0 = readr::read_csv(allfiles[1],show_col_types = FALSE)
   dd1 = dd0 %>% mutate(permutation = 1) %>% mutate(featureRank=as.numeric(rownames(dd0)))
@@ -36,5 +42,9 @@ load_permutated_fws <- function(pathtofile,featureWeightColumnName){
   dd1 = dd1 %>% rename(featureWeight= all_of(featureWeightColumnName))
   dd1 = dd1 %>% dplyr::mutate(LogfeatureWeight = log(featureWeight))
   return(dd1)
+  }
+  else{
+    stop("Input file list is either empty or is not .CSV ; Please check and try again")
+  }
 }
 
